5532138e1018ddcc23cc681234e6f4749dad02aa
chamont <>
2007-09-25 15:30:29 +0000

    Rootification : use TString and Bool_t instead of std::string and bool

--- src/CelManager.cxx
+++ src/CelManager.cxx
@@ -1,169 +1,190 @@
+
 /** 
 * @file CelManager.cxx
 * @brief definition of the class CelManager
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/rootUtil/src/CelManager.cxx,v 1.1 2007/09/19 16:57:05 chamont Exp $
-*  Original author: Heather Kelly heather@lheapop.gsfc.nasa.gov
+* File: $Header: /nfs/slac/g/glast/ground/cvs/rootUtil/rootUtil/CelManager.h,v 1.3 2007/09/25 12:18:33 chamont Exp $
+* Authors:
+*   HK, Heather Kelly, heather@lheapop.gsfc.nasa.gov
+*   DC, David Chamont, LLR, chamont@poly.in2p3.fr
 */
-#ifndef CelManager_cxx
-#define CelManager_cxx
 
-#include "rootUtil/CelManager.h"
-#include "rootUtil/CelEventComponent.h"
-#include "TSystem.h"
-#include <iostream>
+#include <rootUtil/CelManager.h>
+#include <rootUtil/CelEventComponent.h>
+#include <TSystem.h>
+#include <Riostream.h>
 
+
+CelManager::CelManager()
+ : m_verbose(false), 
+   m_fileNameWrite(""), m_fileWrite(0), m_eventCounter(0),
+   m_fileNameRead(""),  m_fileRead(0), m_compChainCol(0), m_masterChain(0)  
+ {}
 
 CelManager::~CelManager() 
-{
-    m_treeCol.clear();
-    if (m_compChainCol) m_compChainCol->Delete();
-    if (m_masterChain) delete m_masterChain;
-}
+ {
+  m_treeCol.clear() ;
+  if (m_compChainCol) m_compChainCol->Delete() ;
+  if (m_masterChain) delete m_masterChain ;
+ }
 
 // Writing Methods 
 
-bool CelManager::initWrite(const std::string &fileName, const std::string &options, bool verbose) {
-    bool stat = true;
-    m_fileNameWrite = fileName;
-    if (gSystem->ExpandPathName(m_fileNameWrite)==kTRUE)
-     {
-      std::cout << "Failed to expand env variable in filename" << std::endl;
-      return false ;
-     }
-    m_outputOptions = options;
-    m_verbose = verbose;
-    return stat;
-}
+Bool_t CelManager::initWrite( const TString & fileName, const TString & options, bool verbose)
+ {
+  Bool_t stat = kTRUE ;
+  m_fileNameWrite = fileName ;
+  if (gSystem->ExpandPathName(m_fileNameWrite)==kTRUE)
+   {
+    std::cout << "Failed to expand env variable in filename" << std::endl;
+    return kFALSE ;
+   }
+  m_outputOptions = options ;
+  m_verbose = verbose ;
+  return stat;
+ }
 
-bool CelManager::initOutputFile() {
-    bool stat = true;
-    m_fileWrite = m_celWrite.makeFile(m_fileNameWrite, m_outputOptions.c_str());
-    if (!m_fileWrite->IsOpen()) {
-        stat = false;
-        std::cout << "CelManager Error opening ROOT file " << m_fileNameWrite << std::endl;
-        return stat;
-    }
-    return stat;
-}
+Bool_t CelManager::initOutputFile()
+ {
+  Bool_t stat = kTRUE ;
+  m_fileWrite = m_celWrite.makeFile(m_fileNameWrite,m_outputOptions.Data()) ;
+  if (!m_fileWrite->IsOpen())
+   {
+    stat = kFALSE ;
+    std::cout << "CelManager Error opening ROOT file " << m_fileNameWrite << std::endl;
+    return stat ;
+   }
+  return stat ;
+ }
 
 
-UInt_t CelManager::addComponent(const std::string &compName, TTree *t) {
-    unsigned int ret = m_celWrite.addComponent(compName);
-    m_treeCol.push_back(t);
-    return ret;
-}
+UInt_t CelManager::addComponent( const TString & compName, TTree * t )
+ {
+  unsigned int ret = m_celWrite.addComponent(compName) ;
+  m_treeCol.push_back(t) ;
+  return ret ;
+ }
 
-bool CelManager::fillEvent() {
-    bool stat = true;
-    try{
-    TDirectory *saveDir = gDirectory;
+Bool_t CelManager::fillEvent()
+ {
+  Bool_t stat = kTRUE ;
+  try
+   {
+    TDirectory * saveDir = gDirectory ;
     // Need to call CompositeEventList::makeFile after the AddComponent calls
-    if (!m_fileWrite)
-        if (!initOutputFile()) throw;
-    if (m_fileWrite->TestBits(TFile::kWriteError)) {
-        throw;
-    }
-    m_fileWrite->cd();
-    std::vector<TTree*>::iterator treeIt;
-    Long64_t numBytes;
-    for (treeIt=m_treeCol.begin(); treeIt != m_treeCol.end(); treeIt++) 
-        numBytes = (*treeIt)->LoadTree(m_eventCounter);
-    m_celWrite.fillEvent(m_treeCol);
-    saveDir->cd();
-    ++m_eventCounter;
-    } catch(...) {
-        std::cerr << "Error filling Meta ROOT file" << std::endl;
-        std::cerr.flush();
-        throw;
-    }
-    return stat;
-}
+    if ((!m_fileWrite)&&(!initOutputFile()))
+     { throw ; }
+    if (m_fileWrite->TestBits(TFile::kWriteError))
+     { throw ; }
+    m_fileWrite->cd() ;
+    std::vector<TTree*>::iterator treeItr ;
+    Long64_t numBytes ;
+    for ( treeItr=m_treeCol.begin() ; treeItr != m_treeCol.end() ; treeItr++ )
+     { numBytes = (*treeItr)->LoadTree(m_eventCounter) ; }
+    m_celWrite.fillEvent(m_treeCol) ;
+    saveDir->cd() ;
+    ++m_eventCounter ;
+   }
+  catch(...)
+   {
+    std::cerr << "Error filling Meta ROOT file" << std::endl ;
+    std::cerr.flush() ;
+    throw ;
+   }
+  return stat ;
+ }
 
-    bool CelManager::fillFileAndTreeSet() {
-        bool stat = true;
-        try {
-            TDirectory *saveDir = gDirectory;
-            m_fileWrite->cd();
-            m_celWrite.fillFileAndTreeSet();
-            m_fileWrite->Write(0,TObject::kWriteDelete);
-            m_fileWrite->Close();
-            saveDir->cd();
-        } catch(...) {
-            std::cerr << "Failed final write to meta ROOT file" << std::endl; 
-            std::cerr.flush(); 
-            throw;
-        }
-        return stat;
-    }
+Bool_t CelManager::fillFileAndTreeSet()
+ {
+  Bool_t stat = kTRUE ;
+  try
+   {
+    TDirectory * saveDir = gDirectory ;
+    m_fileWrite->cd() ;
+    m_celWrite.fillFileAndTreeSet() ;
+    m_fileWrite->Write(0,TObject::kWriteDelete) ;
+    m_fileWrite->Close() ;
+    saveDir->cd() ;
+   }
+  catch(...)
+   {
+    std::cerr << "Failed final write to meta ROOT file" << std::endl ; 
+    std::cerr.flush(); 
+    throw ;
+   }
+  return stat ;
+ }
 
 
 
 /// Reading Methods
 
-bool CelManager::initRead(const std::string &fileName, bool verbose) {
-    bool stat = true;
-    m_fileNameRead = fileName;
-    if (gSystem->ExpandPathName(m_fileNameRead)==kTRUE)
+Bool_t CelManager::initRead( const TString & fileName, Bool_t verbose )
+ {
+  Bool_t stat = kTRUE ;
+  m_fileNameRead = fileName ;
+  if (gSystem->ExpandPathName(m_fileNameRead)==kTRUE)
+   {
+    std::cout << "Failed to expand env variable in filename" << std::endl ;
+    return kFALSE ;
+   }
+  m_verbose = verbose ;
+  m_fileRead = m_celRead.openFile(m_fileNameRead) ;
+  if (!m_fileRead->IsOpen())
+   {
+    stat = kFALSE ;
+    std::cout << "CelManager Error opening ROOT file " << m_fileNameRead << std::endl ;
+    return stat ;
+   }
+
+  // Set up our master TChain and the component TChains
+  if (!m_compChainCol) m_compChainCol = new TObjArray();
+  m_masterChain = m_celRead.buildLinks(m_compChainCol);
+  TIter itr(m_compChainCol);
+  TChain *curChain;
+  while ( (curChain = (TChain*)itr.Next()) )
+   {
+    curChain->SetBranchStatus("*", 1) ;
+    m_chainIndexCol[curChain->GetName()] = curChain->GetTreeIndex() ;
+   }
+  return stat ;
+ }
+
+Long64_t CelManager::getEventIndex( const TString & treeName, Long64_t index)
+ {
+  // make sure the cel index is the active TVirtualIndex
+  setIndex() ;
+  Long64_t retVal = m_masterChain->LoadTree(index) ;  // Causes all component TChains to be loaded correctly via CelIndex
+  if (retVal<0) return retVal ;
+  TChain * compChain = getChainByType(treeName) ;
+  if (!compChain) return -1 ;
+  return (compChain->GetReadEntry()) ;
+ }
+
+int CelManager::setIndex()
+ {
+  int numSet=0 ;
+  std::map<TString, TVirtualIndex*>::iterator mapIt ;
+  for ( mapIt=m_chainIndexCol.begin() ; mapIt != m_chainIndexCol.end() ; mapIt++ )
+   {
+    TChain * curChain = getChainByType(mapIt->first) ;
+    if (curChain)
      {
-      std::cout << "Failed to expand env variable in filename" << std::endl;
-      return false ;
+      curChain->SetTreeIndex(mapIt->second) ;
+      numSet++ ;
      }
-    m_verbose = verbose;
-    m_fileRead = m_celRead.openFile(m_fileNameRead);
-    if (!m_fileRead->IsOpen()) {
-        stat = false;
-        std::cout << "CelManager Error opening ROOT file " << m_fileNameRead << std::endl;
-        return stat;
-    }
+   }
+  return numSet ;
+ }
 
-    // Set up our master TChain and the component TChains
-    if (!m_compChainCol) m_compChainCol = new TObjArray();
-    m_masterChain = m_celRead.buildLinks(m_compChainCol);
-    TIter itr(m_compChainCol);
-    TChain *curChain;
-    while ( (curChain = (TChain*)itr.Next()) ) {
-        curChain->SetBranchStatus("*", 1);
-        m_chainIndexCol[curChain->GetName()] = curChain->GetTreeIndex();
-    }
-
-   
-    return stat;
-}
-
-Long64_t CelManager::getEventIndex(const std::string &treeName, Long64_t index) {
-    // make sure the cel index is the active TVirtualIndex
-    setIndex();
-    Long64_t retVal = m_masterChain->LoadTree(index);  // Causes all component TChains to be loaded correctly via CelIndex
-    if (retVal < 0) return retVal;
-    TChain *compChain = getChainByType(treeName);
-    if (!compChain) return -1;
-    return (compChain->GetReadEntry());
-}
-
-int CelManager::setIndex() {
-    int numSet=0;
-    std::map<std::string, TVirtualIndex*>::iterator mapIt;
-    for (mapIt=m_chainIndexCol.begin();mapIt != m_chainIndexCol.end(); mapIt++) {
-        TChain *curChain = getChainByType(mapIt->first);
-        if (curChain) {
-            curChain->SetTreeIndex(mapIt->second);
-            numSet++;
-        }
-    }
-    return numSet;
-}
+TChain * CelManager::getChainByType( const TString & treeName )
+ {
+  TIter itr(m_compChainCol) ;
+  TChain * curChain ;
+  while ((curChain = (TChain*)itr.Next()))
+   { if (curChain->GetName() == treeName) return curChain ; }
+  return 0 ;
+ }
 
 
-
-TChain* CelManager::getChainByType(const std::string& treeName) {
-    TIter itr(m_compChainCol);
-    TChain *curChain;
-    while ((curChain = (TChain*)itr.Next())) {
-        if (curChain->GetName() == treeName) return curChain;
-    }
-    return 0;
-}
-
-#endif
 
