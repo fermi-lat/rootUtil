bbf463fae2961885c71717f2323a1d09ccc0a80c
ecephas <>
2008-09-10 14:47:55 +0000

    Changed          env.Tool('addLibrary', library = 'rootUtil') To          env.Tool('addLibrary', library = ['rootUtil'])

--- rootUtilLib.py
+++ rootUtilLib.py
@@ -1,7 +1,7 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/rootUtil/rootUtilLib.py,v 1.1 2008/08/15 21:42:39 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = 'rootUtil')
+        env.Tool('addLibrary', library = ['rootUtil'])
     env.Tool('addLibrary', library = env['rootGuiLibs'])
     env.Tool('addLibrary', library = env['minuitLibs'])
 def exists(env):
